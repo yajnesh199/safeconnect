64a28dcb5771ee1a91c29cf28fdbf03b58d13617
NikitaKolamkar1998 <103983643+NikitaKolamkar1998@users.noreply.github.com>
2022-04-19 16:11:42 +0530

    0.0003

--- app/src/main/java/com/example/sample1/MainActivity.java
+++ app/src/main/java/com/example/sample1/MainActivity.java
@@ -5,14 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 Button bt1;
+EditText ed_name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ed_name=findViewById(R.id.ed_name);
         bt1=findViewById(R.id.bt1);
         bt1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +32,12 @@ Button bt1;
             public boolean onLongClick(View view) {
                 Toast.makeText(MainActivity.this, "long click", Toast.LENGTH_SHORT).show();
                 return false;
+            }
+        });
+        ed_name.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Please Your Enter Name", Toast.LENGTH_SHORT).show();
             }
         });
     }
