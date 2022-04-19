b01fc62ac8f2233ed9277de4348a81fd354cf776
NikitaKolamkar1998 <103983643+NikitaKolamkar1998@users.noreply.github.com>
2022-04-19 20:51:32 +0530

    v_1904_n_0.01

--- app/src/main/java/com/example/sample1/Login_Activity.java
+++ app/src/main/java/com/example/sample1/Login_Activity.java
@@ -1,17 +1,35 @@
 package com.example.sample1;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class Login_Activity extends AppCompatActivity {
-
+    LottieAnimationView loginAnimationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        loginAnimationView=findViewById(R.id.login_Animation);
+        loginAnimationView.playAnimation();
+        set_up_action_and_status_bar();
+    }
+    public void set_up_action_and_status_bar() {
+        //hiding the action bar
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.hide();
+        //to get transparent status bar, try changing the themes
+        Window window = getWindow();
+        window.setStatusBarColor(Color.WHITE);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
     }
 }
