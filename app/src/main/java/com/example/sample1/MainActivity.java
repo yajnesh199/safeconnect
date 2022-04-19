af56b0b526106a93bd7b5764a10a15d380b5e6d0
yajnesh199 <103983986+yajnesh199@users.noreply.github.com>
2022-04-19 16:54:14 +0530

    0.3

--- app/src/main/java/com/example/sample1/MainActivity.java
+++ app/src/main/java/com/example/sample1/MainActivity.java
@@ -5,15 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 Button bt1;
+TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bt1=findViewById(R.id.bt1);
+        textView=findViewById(R.id.tt1);
         bt1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
