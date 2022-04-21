566667ce78a5a516c3ee76cba6a98feea5451802
yajnesh199 <103983986+yajnesh199@users.noreply.github.com>
2022-04-21 16:04:46 +0530

    V_2104_y_0.04

--- app/src/main/java/com/example/sample1/ListBluetooth.java
+++ app/src/main/java/com/example/sample1/ListBluetooth.java
@@ -9,15 +9,22 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
 
@@ -29,7 +36,6 @@ public class ListBluetooth extends AppCompatActivity {
     private ArrayList<String> mDeviceList = new ArrayList<String>();
     private BluetoothAdapter mBluetoothAdapter;
     private Button btn;
-
     String tag = "BT_device";
 
     @Override
@@ -39,20 +45,27 @@ public class ListBluetooth extends AppCompatActivity {
         setContentView(R.layout.activity_list_bluetooth);
         // btn = findViewById(R.id.btnGet);
         listView = findViewById(R.id.l1);
-
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        //mBluetoothAdapter.startDiscovery();
-        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-        registerReceiver(mReceiver, filter);
-        Log.e(tag,"init :");
+        set_up_action_and_status_bar();
+        checkpermissions();
     }
-
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==1) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Location Permission Granted", Toast.LENGTH_SHORT).show();
+                discovery();
+            }else{
+                Toast.makeText(this, "Location Permission Diened", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
     @Override
     protected void onDestroy() {
         unregisterReceiver(mReceiver);
         super.onDestroy();
     }
-
     private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
@@ -63,11 +76,37 @@ public class ListBluetooth extends AppCompatActivity {
                     String devicename = device.getName();
                     String macAddress = device.getAddress();
                     mDeviceList.add("" + devicename + "" + macAddress);
-                   // mDeviceList.add(device.getName() + "\n" + device.getAddress());
+                    // mDeviceList.add(device.getName() + "\n" + device.getAddress());
                     Log.e(tag, device.getName() + "\n" + device.getAddress());
                 }
                 listView.setAdapter(new ArrayAdapter<>(context, android.R.layout.simple_list_item_1, mDeviceList));
             }
         }
     };
+    private void set_up_action_and_status_bar() {
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.hide();
+        //to get transparent status bar, try changing the themes
+        Window window = getWindow();
+        window.setStatusBarColor(Color.WHITE);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+    }
+    public void discovery(){
+        if (ActivityCompat.checkSelfPermission(ListBluetooth.this, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+            mBluetoothAdapter.startDiscovery();
+            IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+            registerReceiver(mReceiver, filter);
+            Log.e(tag, "init :");
+            return;
+        }
+    }
+    public boolean checkpermissions(){
+        if (ContextCompat.checkSelfPermission(ListBluetooth.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(ListBluetooth.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            return true;
+        }
+        return false;
+    }
 }
